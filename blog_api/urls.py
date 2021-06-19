1d5515a71ee227bf64fb852602c207674e18f27c
Surya Kiran Chundru <schundru@microsoft.com>
2021-06-19 13:42:14 +0530

    added base project

---
+++ blog_api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+'''from .views import PostList, PostDetail
+'''
+app_name = 'blog_api'
+
+urlpatterns = [
+
+]
+'''
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
+]'''
